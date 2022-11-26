b3d474098b78a0a45b824d09795c442aaea4d931
Paweł Recław <pawel.reclaw@gmail.com>
2022-11-26 12:42:33 +0100

    Dodanie nowej encji do bazy danych

--- src/main/java/pl/sda/hibernate/demo/Main.java
+++ src/main/java/pl/sda/hibernate/demo/Main.java
@@ -1,6 +1,9 @@
 package pl.sda.hibernate.demo;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.time.LocalDate;
 
 /**
  * @author Paweł Recław, AmeN
@@ -9,7 +12,25 @@ import org.hibernate.Session;
  */
 public class Main {
     public static void main(String[] args) {
-        Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession();
-        session.close();
+        // wywołaj try-with-resources który zamknie sesję automatycznie po opuszczeniu try
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
+
+            // Tworzymy studenta
+            Student student = new Student();
+            student.setImie("Paweł");
+            student.setDataUrodzenia(LocalDate.of(1990, 1, 3));
+            student.setKierunekNauczania("Informatyka");
+            student.setIndeks("123123");
+
+            // zapisujemy studenta
+            session.persist(student);
+
+            // zatwierdzamy transakcję
+            transaction.commit();
+        } catch (Exception ioe) {
+            // jeśli złapiemy błąd, to wywoła się catch
+
+        }
     }
 }
---
+++ src/main/java/pl/sda/hibernate/demo/TryCatchPrzykladFinally.java
@@ -0,0 +1,30 @@
+package pl.sda.hibernate.demo;
+
+import org.hibernate.Session;
+
+import java.io.IOException;
+
+/**
+ * @author Paweł Recław, AmeN
+ * @project j133-hibernate-demo
+ * @created 26.11.2022
+ */
+public class TryCatchPrzykladFinally {
+    public static void main(String[] args) {
+//        // wywołaj try
+//        Session session = null;
+//        try {
+//            session = HibernateUtil.INSTANCE.getSessionFactory().openSession();
+//
+//            // jeśli sukces (brak błędu), to nie wywoła się catch
+//        } catch (IOException ioe) {
+//            // jeśli złapiemy błąd, to wywoła się catch
+//
+//        } finally {
+//            // niezależnie od ścieżki wykonania finally musi wykonać się zawsze!
+//            if (session != null) {
+//                session.close();
+//            }
+//        }
+    }
+}
