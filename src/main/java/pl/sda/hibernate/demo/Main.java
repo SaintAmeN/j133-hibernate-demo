15a32b19834c5a82eab7a0e43c6442047ca1b614
Paweł Recław <pawel.reclaw@gmail.com>
2022-11-26 11:36:53 +0100

    Baza skonfigurowana

--- src/main/java/pl/sda/hibernate/demo/Main.java
+++ src/main/java/pl/sda/hibernate/demo/Main.java
@@ -1,5 +1,7 @@
 package pl.sda.hibernate.demo;
 
+import org.hibernate.Session;
+
 /**
  * @author Paweł Recław, AmeN
  * @project j133-hibernate-demo
@@ -7,6 +9,7 @@ package pl.sda.hibernate.demo;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession();
+        session.close();
     }
 }
