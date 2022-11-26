3125413a91502256ac58853f3959ee2f5c8fd1ee
Paweł Recław <pawel.reclaw@gmail.com>
2022-11-26 10:08:10 +0100

    Drugi commit

---
+++ src/main/java/pl/sda/hibernate/demo/Main.java
@@ -0,0 +1,12 @@
+package pl.sda.hibernate.demo;
+
+/**
+ * @author Paweł Recław, AmeN
+ * @project j133-hibernate-demo
+ * @created 26.11.2022
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
